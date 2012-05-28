e08c9b9645b79a8c6c14aef85d97c222f51c29f1
Grzegorz Brzezinka <grzegorz@brzezinka.eu>
2012-05-28 21:52:26 +0200

    added bg image for taxon

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -1,6 +1,13 @@
 Spree::Taxon.class_eval do
   
-  attr_accessible :page_id, :visible
+  attr_accessible :page_id, :visible, :bg_image
   belongs_to :page
+  
+  has_attached_file :bg_image,
+      :styles => {:normal => '256x256>' },
+      :default_style => :normal,
+      :url => '/spree/taxons_bg/:id/:style/:basename.:extension',
+      :path => ':rails_root/public/spree/taxons_bg/:id/:style/:basename.:extension',
+      :default_url => '/assets/default_taxon_bg.png'
   
 end
--- spree_extended_taxon.gemspec
+++ spree_extended_taxon.gemspec
@@ -2,7 +2,7 @@
 Gem::Specification.new do |s|
   s.platform    = Gem::Platform::RUBY
   s.name        = 'spree_extended_taxon'
-  s.version     = '1.1.1'
+  s.version     = '1.1.2'
   s.summary     = 'TODO: Add gem summary here'
   s.description = 'TODO: Add (optional) gem description here'
   s.required_ruby_version = '>= 1.8.7'
