fe629fc486e42579a30d4807f83fe2b515a89a24
Mariusz Henn <mariusz.henn@gmail.com>
2012-06-25 20:55:12 +0000

    taxon redirect option

--- db/migrate/20120526092638_add_fields_to_taxons.rb
+++ db/migrate/20120526092638_add_fields_to_taxons.rb
@@ -2,11 +2,9 @@ class AddFieldsToTaxons < ActiveRecord::Migration
     def self.up
       add_column :spree_taxons, :page_id, :integer
       add_column :spree_taxons, :visible, :boolean
-      add_column :spree_taxons, :page_redirect, :text
     end
 
     def self.down
-      remove_column :spree_taxons, :page_redirect
       remove_column :spree_taxons, :page_id
       remove_column :spree_taxons, :visible, :default => true
     end
